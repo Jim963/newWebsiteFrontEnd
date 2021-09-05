ec73123a63b3cf81d511931ca79e95631aad2b7e
Jim963 <qu4ej3w.6@gmail.com>
2021-09-06 02:10:58 +0800

    axios請求ip換成server的

--- assets/axios/index.js
+++ assets/axios/index.js
@@ -2,17 +2,17 @@ import axios from "axios";
 
 // 全局配置
 //如果前後端同一包用這一個
-const instance = axios.create({
-  baseURL: "http://localhost:81",
-  timeout: 10000,
-  headers: {},
-});
-
-//前後端分開跑
 // const instance = axios.create({
-//   baseURL: "http://202.182.124.162/:81",
+//   baseURL: "http://localhost:81",
 //   timeout: 10000,
 //   headers: {},
 // });
+
+//前後端分開跑
+const instance = axios.create({
+  baseURL: "http://202.182.124.162/:81",
+  timeout: 10000,
+  headers: {},
+});
 
 export default instance;
