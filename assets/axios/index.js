b33b720bfe6eeb33d6ec5e2ea6f8512caa379d0c
Jim963 <qu4ej3w.6@gmail.com>
2021-09-06 00:36:11 +0800

    變更axios請求ip

--- assets/axios/index.js
+++ assets/axios/index.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 
 // 全局配置
+//如果前後端同一包用這一個
+// const instance = axios.create({
+//   baseURL: "http://localhost:81",
+//   timeout: 10000,
+//   headers: {},
+// });
+
+//前後端分開跑
 const instance = axios.create({
-  baseURL: "http://localhost:81",
+  baseURL: "http://202.182.124.162/:81",
   timeout: 10000,
   headers: {},
 });
--- plugins/config/index.js
+++ plugins/config/index.js
@@ -1,6 +1,12 @@
+// export default (context, inject) => {
+//   inject('configSet', {
+//        baseUrl : 'http://localhost:81'
+//   });
+// }
+
 export default (context, inject) => {
   inject('configSet', {
-       baseUrl : 'http://localhost:81'
+       baseUrl : 'http://202.182.124.162/:81'
   });
 }
 
