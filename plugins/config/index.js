834da277d5f339a6017751e48d3c882ffa2c716c
Jim963 <qu4ej3w.6@gmail.com>
2021-09-06 00:43:36 +0800

    axios改回localhost

--- plugins/config/index.js
+++ plugins/config/index.js
@@ -1,12 +1,8 @@
-// export default (context, inject) => {
-//   inject('configSet', {
-//        baseUrl : 'http://localhost:81'
-//   });
-// }
-
 export default (context, inject) => {
   inject('configSet', {
-       baseUrl : 'http://202.182.124.162/:81'
+       baseUrl : 'http://localhost:81'
   });
 }
+
+
 
